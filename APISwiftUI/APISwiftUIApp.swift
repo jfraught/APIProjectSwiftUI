e33e45795600b55e8be0b08351479f8fbedefd0a
Jordan Fraughton <fraughtdawg@gmail.com>
2024-06-04 07:36:09 -0600

    Initial Commit

---
+++ APISwiftUI/APISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APISwiftUIApp.swift
+//  APISwiftUI
+//
+//  Created by Jordan Fraughton on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct APISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
